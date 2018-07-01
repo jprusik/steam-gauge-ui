f3f675a5f5e353bc545b67b53d4755e9ef1b1855
Jon Prusik <jprusik@classynemesis.com>
2018-07-01 14:52:27 -0400

    add Steam Receipt page

--- src/components/appBody.js
+++ src/components/appBody.js
@@ -6,6 +6,7 @@ import HomePage from '../pages/home';
 import AccountPage from '../pages/account';
 import AboutPage from '../pages/about';
 import PrivacyPage from '../pages/privacy';
+import ReceiptPage from '../pages/receipt';
 
 const urlRedirect = (url) => {
   return () => window.location = url;
@@ -20,6 +21,7 @@ const AppBody = () => {
       <Route path="/account" component={ AccountPage } />
       <Route path="/about" component={ AboutPage } />
       <Route path="/privacy" component={ PrivacyPage } />
+      <Route path="/receipt" component={ ReceiptPage } />
       <Route path="/login" component={ urlRedirect(loginUrl) }/>
     </div>
   );
