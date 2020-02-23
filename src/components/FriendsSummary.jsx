b7d5a51daa0980050b6ffbe3f9415fc71f21f365
Jon Prusik <jprusik@classynemesis.com>
2020-02-23 10:22:29 -0500

    fix and update FriendsSummary

--- src/components/FriendsSummary.jsx
+++ src/components/FriendsSummary.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {steamFriendsShareTwitter} from '../constants/urls';
 import {
   groupMultiplayerAppsByAccountsOwnedByCount,
   groupMultiplayerAppsByAccountsPlaytime
 } from '../utils/friends';
+import ShareBar from './ShareBar';
 import './FriendsSummary.scss';
 
 const FriendsSummary = ({userId, accounts = [], multiplayerApps = []}) => {
@@ -23,47 +23,53 @@ const FriendsSummary = ({userId, accounts = [], multiplayerApps = []}) => {
   const userFriends = accounts.filter(account => account.steamid !== userId);
   const userFriendsCount = userFriends.length;
 
+  // Apps by owned count
   const totalOwnedAppsCounts = groupMultiplayerAppsByAccountsOwnedByCount({
     userFriends,
     userApps,
     multiplayerApps
   });
 
-  const mostOwnedCommonAppId = Object.keys(totalOwnedAppsCounts)
+  const totalOwnedAppsCountsIds = Object.keys(totalOwnedAppsCounts);
+
+  const mostOwnedCommonAppId = totalOwnedAppsCountsIds
     .reduce((mostOwnedId, appId) =>
       totalOwnedAppsCounts[appId] > totalOwnedAppsCounts[mostOwnedId]
       ? appId
       : mostOwnedId
-    , {});
+    , totalOwnedAppsCountsIds[0]);
 
   const mostOwnedCommonApp = userApps.find(({appid}) => `${appid}` === mostOwnedCommonAppId);
 
-  const leastOwnedCommonAppId = Object.keys(totalOwnedAppsCounts)
+  const leastOwnedCommonAppId = totalOwnedAppsCountsIds
     .reduce((mostOwnedId, appId) =>
       totalOwnedAppsCounts[appId] < totalOwnedAppsCounts[mostOwnedId]
       ? appId
       : mostOwnedId
-    , {});
+    , totalOwnedAppsCountsIds[0]);
 
   const leastOwnedCommonApp = userApps.find(({appid}) => `${appid}` === leastOwnedCommonAppId);
 
+  // Apps by playtime
   const totalOwnedAppsPlaytime = groupMultiplayerAppsByAccountsPlaytime({
     userFriends,
     userApps,
     multiplayerApps
   });
 
-  const highestAveragePlaytimeCommonAppId = Object.keys(totalOwnedAppsPlaytime)
+  const totalOwnedAppsPlaytimeIds = Object.keys(totalOwnedAppsPlaytime);
+
+  const highestAveragePlaytimeCommonAppId = totalOwnedAppsPlaytimeIds
     .reduce((mostPlayedOnAverageId, appId) =>
       (totalOwnedAppsPlaytime[appId]/userFriendsCount) > (totalOwnedAppsPlaytime[mostPlayedOnAverageId]/userFriendsCount)
       ? appId
       : mostPlayedOnAverageId
-    , {});
+    , totalOwnedAppsPlaytimeIds[0]);
 
   const highestAveragePlaytimeCommonApp = userApps.find(({appid}) => `${appid}` === highestAveragePlaytimeCommonAppId);
 
   return (
-    <div key="content-head" className="jumbotron">
+    <div className="jumbotron">
       <img className="useravatar" alt="user avatar" src={userImage}/>
       <h3 className="userhead">
         <a href={userUrl} target="_blank" rel="noopener noreferrer">{userUsername}</a> and {userFriendsCount} friends have these multiplayer games in common:
@@ -87,7 +93,10 @@ const FriendsSummary = ({userId, accounts = [], multiplayerApps = []}) => {
           </div>
         }
         <div className="col-xs-6">
-          <strong>Share:</strong> <a className="custom-share twitter" href={steamFriendsShareTwitter(userId)}><i className="fa fa-twitter"></i></a>
+          <ShareBar
+            message={`I just found out what multiplayer games my ${userFriendsCount} friends and I have in common by using MySteamGauge.com`}
+            url={`${process.env.REACT_APP_DOMAIN_URL}/friends/${userId}`}
+          />
         </div>
       </div>
     </div>
